e96f5e81e8f9cca1b9f9eb31188552226b60c826
chaitanyakasaraneni <chaitanyakrishna755@gmail.com>
2020-06-17 02:17:42 -0700

    edited db details

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 
 database_name = "capstone"
 user_name = "postgres"
-password = ""
+password = "4062a6c035176a481c4b81bcd53658fb53ca27671149edd1ec3bcd16322ced46"
 database_path = "postgres://{}:{}@{}/{}".format(
   user_name,
   password,
